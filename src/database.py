1e70c33987b2c128145ee3fc82c7b00dd79f8910
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-09-07 15:36:32 -0300

    fix: multiple links in list
    feat: save updated link

--- src/database.py
+++ src/database.py
@@ -1,13 +1,11 @@
-from webbrowser import get
 import requests
-import json
+from json import dumps
 
 from link import *
 
 class Database():
     def __init__(self, url):
         self.url = url
-        _connection = requests.get(self.url)
 
     def _clear_database_(self):
         clear_request = requests.delete(f'{self.url}/links/.json')
@@ -18,9 +16,13 @@ class Database():
         object_link.load_json(json)
         return object_link
 
-    def get_all_links(self) -> dict:
-        links = requests.get(f'{self.url}/links.json')
-        return links.json()
+    def get_all_links(self) -> list:
+        links = requests.get(f'{self.url}/links/.json')
+        list_links = []
+        for link in links.json():
+            list_links.append(self.json_to_link({link: links.json()[link]}))
+
+        return list_links
 
     def get_link_by_keyword(self, keyword: str) -> Link:
         link = requests.get(f'{self.url}/links/{keyword}/.json')
@@ -29,20 +31,27 @@ class Database():
     def get_link_by_long(self, long: str) -> Link:
         links = self.get_all_links()
         for link in links:
-            if links[link]["long_url"] == long:
-                return self.json_to_link({link: links[link]})
+            if link.long_url == long:
+                return link
 
     def get_links_by_tag(self, tag: str) -> list:
         links = self.get_all_links()
         links_with_tag = []
         for link in links:
-            if tag in links[link]["tags"]:
-                links_with_tag.append(self.json_to_link({link: links[link]}))
+            if tag in link.tags:
+                links_with_tag.append(link)
         return links_with_tag
 
     def create_link(self, link: Link):
+        keyword = link.keyword
         link_data = link.to_dict()
-        keyword = list(link_data)[0]
         link_data = link_data.get(keyword)
-        create_request = requests.put(f'{self.url}/links/{keyword}/.json', data=json.dumps(link_data))
+        create_request = requests.put(f'{self.url}/links/{keyword}/.json', data=dumps(link_data))
         return create_request.status_code
+
+    def save_link(self, link: Link):
+        keyword = link.keyword
+        link_data = link.to_dict()
+        link_data = link_data.get(keyword)
+        save_request = requests.patch(f'{self.url}/links/{keyword}/.json', data=dumps(link_data))
+        return save_request.status_code
