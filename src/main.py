636c1d3150174e357350eacac214d37de4c9b2c2
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-09-07 13:00:51 -0300

    feat: tag filter

--- src/main.py
+++ src/main.py
@@ -5,5 +5,26 @@ from database import *
 from os import environ
 db = Database(environ['databaseURL'])
 
-print(db.create_link(Link('https://google.com', 'google', ['all'], 0, 0)))
-print(db.get_link_by_keyword('google').long_url)
+test_links = [
+    Link('https://google.com', 'google', ['search', 'google'], 0, 0),
+    Link('https://youtube.com', 'youtube', ['video', 'youtube'], 0, 0),
+    Link('https://github.com', 'github', ['code', 'github'], 0, 0),
+    Link('https://stackoverflow.com', 'stackoverflow', ['code', 'stackoverflow'], 0, 0),
+    Link('https://reddit.com', 'reddit', ['social', 'reddit'], 0, 0),
+    Link('https://twitter.com', 'twitter', ['social', 'twitter'], 0, 0),
+    Link('https://facebook.com', 'facebook', ['social', 'facebook'], 0, 0),
+    Link('https://instagram.com', 'instagram', ['social', 'instagram'], 0, 0),
+    Link('https://twitch.tv', 'twitch', ['video', 'twitch'], 0, 0),
+    Link('https://netflix.com', 'netflix', ['video', 'netflix'], 0, 0),
+    Link('https://amazon.com', 'amazon', ['shopping', 'amazon'], 0, 0),
+    Link('https://wikipedia.org', 'wikipedia', ['search', 'wikipedia'], 0, 0)
+]
+
+for link in test_links:
+    db.create_link(link)
+
+
+social_tag = db.get_links_by_tag('social')
+
+for link in social_tag:
+    print(link.keyword)
