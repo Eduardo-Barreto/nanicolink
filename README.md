1407e6d5c8824b2979368d1e366062f8195e7eaf
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-09-08 01:29:33 -0300

    feat: start API

--- README.md
+++ README.md
@@ -3,3 +3,56 @@ Pílulas de nanicolina (Chapolin) para o seu link longo!
 
 tasks
 https://eduardo-barreto.notion.site/8cc5b41333464993864d1144c0d503a8?v=fbeeaf16fde243a2a26b73762c3474f2
+
+# Requisições
+
+## Criar um link
+- url: `host/create`
+- método: `POST`
+
+Ao criar um link, você pode personalizar os seguintes itens:
+- keyword: a palavra-chave que será usada para acessar o link (ex: host/keyword)
+- url: o link que será encurtado
+- tags: tags para o link
+- destroy_clicks: a quantidade de cliques que o link pode receber antes de ser destruído (0 para infinito)
+- destroy_time: o tempo em horas que o link pode ficar ativo antes de ser destruído (0 para infinito)
+```json
+{
+    "long_url": "https://google.com",
+    "keyword": "google",
+    "tags": ["search", "google"],
+    "destroy_clicks": 0,
+    "destroy_time": 0
+}
+```
+
+Você também pode optar por usar as configurações padrão, para isso basta passar somente o link no request
+```json
+{
+    "long_url": "https://google.com",
+}
+```
+
+## Deletar um link
+- url: `host/delete`
+- método: `POST`
+
+Para deletar um link, você precisa passar a keyword dele
+```json
+{
+    "keyword": "google"
+}
+```
+
+## Acessar um link
+- url: `host/[keyword]`
+- método: `GET`
+Ao acessar um link, você é redirecionado.
+
+## Listar links por tag
+- url: `host/tag/[tag]`
+- método: `GET`
+
+# Acessar detalhes de um link
+- url: `host/[keyword]/details`
+- método: `GET`
--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from os import environ
+import uvicorn
+from fastapi import FastAPI, HTTPException, Body, Request
+from starlette.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+from time import time
+from typing import Optional
+from pydantic import BaseModel
 
 from link import Link
 from database import Database
@@ -6,38 +13,150 @@ from database import Database
 db = Database(environ['databaseURL'])
 
 
-def create_test_links():
-    db._clear_database_()
-    test_links = [
-        Link('https://google.com', 'google', ['search', 'google'], 0, 0),
-        Link('https://youtube.com', 'youtube', ['video', 'youtube'], 0, 0),
-        Link('https://github.com', 'github', ['code', 'github'], 0, 0),
-        Link(
-            'https://stackoverflow.com',
-            'stackoverflow',
-            ['code', 'stackoverflow'],
-            0,
-            0,
-        ),
-        Link('https://reddit.com', 'reddit', ['social', 'reddit'], 0, 0),
-        Link('https://twitter.com', 'twitter', ['social', 'twitter'], 0, 0),
-        Link('https://facebook.com', 'facebook', ['social', 'facebook'], 0, 0),
-        Link('https://instagram.com', 'instagram',
-             ['social', 'instagram'], 0, 0),
-        Link('https://twitch.tv', 'twitch', ['video', 'twitch'], 0, 0),
-        Link('https://netflix.com', 'netflix', ['video', 'netflix'], 0, 0),
-        Link('https://amazon.com', 'amazon', ['shopping', 'amazon'], 0, 0),
-        Link('https://wikipedia.org', 'wikipedia',
-             ['search', 'wikipedia'], 0, 0),
-    ]
+def set_response(status_code, message):
+    return {'status_code': status_code, 'message': message}
 
-    for link in test_links:
+
+class LinkModel(BaseModel):
+    long_url: str
+    keyword: Optional[str] = ''
+    tags: Optional[list] = []
+    destroy_clicks: Optional[int] = 0
+    destroy_time: Optional[int] = 0
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return set_response(200, 'Bem vindo! Página inicial')
+
+
+@app.get('/{keyword}')
+async def redirect(keyword: str):
+    if not db.keyword_exists(keyword):
+        raise HTTPException(
+            404,
+            f'Não foi possível encontrar um link com a keyword {keyword}'
+        )
+
+    link = db.get_link_by_keyword(keyword)
+    link.clicks += 1
+    db.save_link(link)
+
+    if link.destroy_time > 0:
+        if link.date_created + link.destroy_time*3600 <= time():
+            try:
+                db.delete_link(link)
+            except Exception as e:
+                raise HTTPException(500, 'Erro ao deletar link: ' + str(e))
+
+            raise HTTPException(410, 'Link expirado')
+
+    if link.destroy_clicks > 0 and link.clicks >= link.destroy_clicks:
+        db.delete_link(link)
+
+    return RedirectResponse(url=link.long_url)
+
+
+@app.get('/{keyword}/details')
+async def get_link_details(keyword: str):
+    if not db.keyword_exists(keyword):
+        raise HTTPException(
+            404,
+            f'Não foi possível encontrar um link com a keyword {keyword}'
+        )
+
+    try:
+        link = db.get_link_by_keyword(keyword)
+    except Exception as e:
+        raise HTTPException(500, 'Erro ao buscar link: ' + str(e))
+
+    return set_response(200, link)
+
+
+@app.get('/tag/{tag}')
+async def get_links_by_tag(tag: str):
+    try:
+        links = db.get_links_by_tag(tag)
+    except Exception as e:
+        raise HTTPException(500, 'Erro ao buscar links: ' + str(e))
+
+    return set_response(200, links)
+
+
+@app.put('/create')
+async def create_link(
+    link: LinkModel = Body(
+        examples={
+            "Normal": {
+                "summary": "Cria um link padrão, sem personalização",
+                "value": {
+                    "long_url": "https://google.com",
+                }
+            },
+            "totalmente personalizado": {
+                "summary": "Cria um link totalmente personalizado",
+                "value": {
+                    "long_url": "https://google.com",
+                    "keyword": "google",
+                    "tags": ["search", "google"],
+                    "destroy_clicks": 0,
+                    "destroy_time": 0
+                }
+            }
+        }
+    )
+):
+    if db.keyword_exists(link.keyword):
+        raise HTTPException(409, f'A keyword {link.keyword} já existe')
+
+    link = Link(
+        link.long_url,
+        link.keyword,
+        link.tags,
+        link.destroy_clicks,
+        link.destroy_time
+    )
+    try:
         db.create_link(link)
+    except Exception as e:
+        raise HTTPException(500, 'Erro ao criar link' + str(e))
+
+    return set_response(201, 'Link criado com sucesso')
 
 
-create_test_links()
+@app.delete('/delete')
+async def delete_link(request: Request):
+    keyword = await request.json()
+    keyword = keyword.get('keyword')
 
-social_tag = db.get_links_by_tag('social')
+    if not db.keyword_exists(keyword):
+        raise HTTPException(
+            404,
+            f'Não foi possível encontrar um link com a keyword {keyword}'
+        )
 
-for link in social_tag:
-    print(link.keyword)
+    link = db.get_link_by_keyword(keyword)
+    try:
+        db.delete_link(link)
+    except Exception as e:
+        raise HTTPException(500, 'Erro ao deletar link: ' + str(e))
+
+    return set_response(200, 'Link deletado com sucesso')
+
+
+def main():
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+    uvicorn.run(app)
+
+
+if __name__ == '__main__':
+    main()
