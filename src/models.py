721154133362f7650c18d6354d95195b139b0464
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-09-11 14:06:17 -0300

    fix: organized models

---
+++ src/models.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class RequestLink(BaseModel):
+    long_url: str
+    keyword: Optional[str] = ''
+    tags: Optional[list] = []
+    destroy_clicks: Optional[int] = 0
+    destroy_time: Optional[int] = 0
+
+
+class Keyword(BaseModel):
+    keyword: str
+
+
+class ResponseLink(BaseModel):
+    long_url: str
+    keyword: str
+    clicks: int
+    destroy_clicks: int
+    tags: list
+    date_created: float
+    destroy_time: int
